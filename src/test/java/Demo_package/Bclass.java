d3c5ad7fd9cab6b284ab5fb3f7381b2853c56b96
Pavani Pondugula <Pavani Pondugula@LAPTOP-M3RB8FCH>
2023-12-09 12:17:14 +0530

    11 comitt

---
+++ src/test/java/Demo_package/Bclass.java
@@ -0,0 +1,19 @@
+package Demo_package;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class Bclass {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
